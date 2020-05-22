649d9a64fa16300cbd878ae3f30356020a60c1f1
marlonjf <marlon.jf12@gmail.com>
2020-05-21 22:57:40 -0300

    fix

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.post('/register', (req, res) => {
     .then(trx.commit)
     .catch(trx.rollback)
   })
-  .catch(err => res.status(400).json('unable to register'))
+    .catch(err => res.status(400).json(err))
 })
 
 app.listen(process.env.PORT || 3000, () => {
